85b8574735b7a7dfa0eeab6164ccaa7fe100ef3a
Rayyan Waqar <167600932+rayyme6@users.noreply.github.com>
2025-10-02 19:20:00 +0500

    added the example3

--- example3.c
+++ example3.c
@@ -1,16 +1,31 @@
 /**********************************************************************
-Finds the 150 best features in an image and tracks them through the 
-next two images.  The sequential mode is set in order to speed
-processing.  The features are stored in a feature table, which is then
-saved to a text file; each feature list is also written to a PPM file.
+Tracks features across *all* sequentially numbered images (img0.pgm,
+img1.pgm, img2.pgm, ... until no more files are found). Resizes images 
+to a larger size to make computation heavier for profiling.
 **********************************************************************/
 
 #include <stdlib.h>
 #include <stdio.h>
+#include <string.h>
 #include "pnmio.h"
 #include "klt.h"
 
-/* #define REPLACE */
+/* Resize image to larger resolution (simple nearest-neighbor scaling) */
+unsigned char* resizeImage(unsigned char* src, int srcCols, int srcRows, 
+                           int scale, int* dstCols, int* dstRows) {
+    *dstCols = srcCols * scale;
+    *dstRows = srcRows * scale;
+    unsigned char* dst = (unsigned char*) malloc((*dstCols) * (*dstRows) * sizeof(unsigned char));
+
+    for (int r = 0; r < *dstRows; r++) {
+        for (int c = 0; c < *dstCols; c++) {
+            int srcR = r / scale;
+            int srcC = c / scale;
+            dst[r * (*dstCols) + c] = src[srcR * srcCols + srcC];
+        }
+    }
+    return dst;
+}
 
 #ifdef WIN32
 int RunExample3()
@@ -18,49 +33,74 @@ int RunExample3()
 int main()
 #endif
 {
-  unsigned char *img1, *img2;
-  char fnamein[100], fnameout[100];
-  KLT_TrackingContext tc;
-  KLT_FeatureList fl;
-  KLT_FeatureTable ft;
-  int nFeatures = 150, nFrames = 10;
-  int ncols, nrows;
-  int i;
+    unsigned char *img1, *img2, *resized1, *resized2;
+    char fnamein[100], fnameout[100];
+    KLT_TrackingContext tc;
+    KLT_FeatureList fl;
+    KLT_FeatureTable ft;
+    int nFeatures = 300;   /* more features since images are larger */
+    int nFrames = 0;
+    int ncols, nrows, rcols, rrows;
+    int scale = 2;         /* resize factor */
+    int i;
 
-  tc = KLTCreateTrackingContext();
-  fl = KLTCreateFeatureList(nFeatures);
-  ft = KLTCreateFeatureTable(nFrames, nFeatures);
-  tc->sequentialMode = TRUE;
-  tc->writeInternalImages = FALSE;
-  tc->affineConsistencyCheck = -1;  /* set this to 2 to turn on affine consistency check */
- 
-  img1 = pgmReadFile("img0.pgm", NULL, &ncols, &nrows);
-  img2 = (unsigned char *) malloc(ncols*nrows*sizeof(unsigned char));
+    tc = KLTCreateTrackingContext();
+    fl = KLTCreateFeatureList(nFeatures);
 
-  KLTSelectGoodFeatures(tc, img1, ncols, nrows, fl);
-  KLTStoreFeatureList(fl, ft, 0);
-  KLTWriteFeatureListToPPM(fl, img1, ncols, nrows, "feat0.ppm");
+    tc->sequentialMode = TRUE;
+    tc->writeInternalImages = FALSE;
+    tc->affineConsistencyCheck = -1;
 
-  for (i = 1 ; i < nFrames ; i++)  {
-    sprintf(fnamein, "img%d.pgm", i);
-    pgmReadFile(fnamein, img2, &ncols, &nrows);
-    KLTTrackFeatures(tc, img1, img2, ncols, nrows, fl);
+    /* Load first image */
+    sprintf(fnamein, "img0.pgm");
+    img1 = pgmReadFile(fnamein, NULL, &ncols, &nrows);
+    resized1 = resizeImage(img1, ncols, nrows, scale, &rcols, &rrows);
+
+    /* Prepare feature table, we don’t know nFrames yet so overallocate */
+    ft = KLTCreateFeatureTable(1000, nFeatures);  /* large enough upper bound */
+
+    /* Select good features on first image */
+    KLTSelectGoodFeatures(tc, resized1, rcols, rrows, fl);
+    KLTStoreFeatureList(fl, ft, 0);
+    KLTWriteFeatureListToPPM(fl, resized1, rcols, rrows, "feat0.ppm");
+    nFrames = 1;
+
+    /* Process subsequent images until file not found */
+    for (i = 1; ; i++) {
+        sprintf(fnamein, "img%d.pgm", i);
+        FILE* f = fopen(fnamein, "rb");
+        if (!f) break;  /* stop when no more files */
+        fclose(f);
+
+        img2 = pgmReadFile(fnamein, NULL, &ncols, &nrows);
+        resized2 = resizeImage(img2, ncols, nrows, scale, &rcols, &rrows);
+
+        KLTTrackFeatures(tc, resized1, resized2, rcols, rrows, fl);
 #ifdef REPLACE
-    KLTReplaceLostFeatures(tc, img2, ncols, nrows, fl);
+        KLTReplaceLostFeatures(tc, resized2, rcols, rrows, fl);
 #endif
-    KLTStoreFeatureList(fl, ft, i);
-    sprintf(fnameout, "feat%d.ppm", i);
-    KLTWriteFeatureListToPPM(fl, img2, ncols, nrows, fnameout);
-  }
-  KLTWriteFeatureTable(ft, "features.txt", "%5.1f");
-  KLTWriteFeatureTable(ft, "features.ft", NULL);
+        KLTStoreFeatureList(fl, ft, i);
 
-  KLTFreeFeatureTable(ft);
-  KLTFreeFeatureList(fl);
-  KLTFreeTrackingContext(tc);
-  free(img1);
-  free(img2);
+        sprintf(fnameout, "feat%d.ppm", i);
+        KLTWriteFeatureListToPPM(fl, resized2, rcols, rrows, fnameout);
 
-  return 0;
+        free(resized1);
+        free(img1);
+        resized1 = resized2;
+        img1 = img2;
+        nFrames++;
+    }
+
+    /* Write out feature table */
+    KLTWriteFeatureTable(ft, "features.txt", "%5.1f");
+    KLTWriteFeatureTable(ft, "features.ft", NULL);
+
+    KLTFreeFeatureTable(ft);
+    KLTFreeFeatureList(fl);
+    KLTFreeTrackingContext(tc);
+    free(resized1);
+    free(img1);
+
+    return 0;
 }
 
